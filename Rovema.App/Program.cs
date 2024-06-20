9551325bd90f9c6b1c3e22ab87acddfb1b64f162
Liluyoud Cury de Lacerda <liluyoud@hotmail.com>
2024-06-20 17:48:34 -0400

    app v0.1

--- Rovema.App/Program.cs
+++ Rovema.App/Program.cs
@@ -5,6 +5,7 @@ using Blazored.LocalStorage;
 using Dclt.Directus;
 using Rovema.App;
 using MudBlazor.Services;
+using Microsoft.AspNetCore.Authorization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -12,6 +13,11 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+//builder.Services.AddAuthorizationCore(options => {
+//    options.FallbackPolicy = new AuthorizationPolicyBuilder()
+//        .RequireAuthenticatedUser()
+//        .Build();
+//});
 builder.Services.AddAuthorizationCore();
 builder.Services.AddHttpClient();
 
